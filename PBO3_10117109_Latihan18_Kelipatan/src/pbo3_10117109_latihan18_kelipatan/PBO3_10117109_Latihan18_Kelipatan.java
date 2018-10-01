e7c576b78fa6c1f722550ccda20709044311711b
Stefanus17 <43497735+Stefanus17@users.noreply.github.com>
2018-10-01 21:18:25 +0700

    Add files via upload

---
+++ PBO3_10117109_Latihan18_Kelipatan/src/pbo3_10117109_latihan18_kelipatan/PBO3_10117109_Latihan18_Kelipatan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117109_latihan18_kelipatan;
+
+/**
+ * Nama  : Stefanus Saputra
+ * NIM   : 10117109
+ * Kelas : IF 3
+ * Deskripsi : Membuat program kelipatan 
+ */
+public class PBO3_10117109_Latihan18_Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (Double i = 3.5; i<=35; i=i+3.5) {
+        System.out.println(i);    
+        }        
+    }
+    
+}
